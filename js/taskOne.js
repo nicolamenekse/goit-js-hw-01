e469fd0d3dda45015bae80cf4e5b658961ad4f5f
Nicola <menekse776@gmail.com>
2024-11-06 02:37:53 +0300

    js first homeWork

---
+++ js/taskOne.js
@@ -0,0 +1,9 @@
+function makeTransaction(quantity, pricePerDroid) {
+    console.log(`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`);
+
+}
+
+makeTransaction(10, 20)
+makeTransaction(25,32)
+makeTransaction(511,23)
+
---
+++ js/taskThree.js
@@ -0,0 +1,8 @@
+function getElementWidth(content,padding,border){
+    const total = content + padding * 2 + border * 2 
+    console.log(`${total} px ` );
+}
+
+getElementWidth(20,21,22)
+getElementWidth(1451,124,23)
+getElementWidth(123,123,423)
